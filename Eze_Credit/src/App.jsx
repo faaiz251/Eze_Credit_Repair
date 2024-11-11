f78262dedcf8c55973732e8aa8a1f8b744211259
faaiz251 <mohdfaaizazeem@gmail.com>
2024-11-12 01:20:40 +0530

    first commit

--- Eze_Credit/src/App.jsx
+++ Eze_Credit/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Home} from './components/Home';
+import {Signup} from './components/Signup';
+import {Signin} from './components/Signin';
+import {About}  from './components/About';
 
 function App() {
-
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
